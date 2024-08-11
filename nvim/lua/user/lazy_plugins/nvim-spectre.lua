4bbfb665a1040a4318b8041177295c3e5064503e
Andrew Chikin <chikin.ap@phystech.edu>
2024-08-11 16:32:47 +0300

    modified plugins: spectre, oil

--- nvim/lua/user/lazy_plugins/nvim-spectre.lua
+++ nvim/lua/user/lazy_plugins/nvim-spectre.lua
@@ -1,14 +1,21 @@
 return {
     "nvim-pack/nvim-spectre",
     config = function()
-        -- require("spectre").setup()
-        vim.keymap.set("n", "<leader>ss", "<cmd>Spectre toggle<CR>", {
+        local spectre = require("spectre")
+
+        vim.keymap.set("n", "<leader>ss", function()
+            spectre.toggle()
+        end, {
             desc = "Toggle Spectre",
         })
-        vim.keymap.set("n", "<leader>sw", "<cmd>Spectre open_visual({select_word=true})<CR>", {
+        vim.keymap.set("n", "<leader>sw", function()
+            spectre.open_visual({ select_word = true })
+        end, {
             desc = "Search current word",
         })
-        vim.keymap.set("n", "<leader>sf", "<cmd>Spectre open_file_search({select_word=true})<CR>", {
+        vim.keymap.set("n", "<leader>sf", function()
+            spectre.open_file_search({ select_word = true })
+        end, {
             desc = "Search on current file",
         })
     end,
--- nvim/lua/user/plugins-config/oil/functions.lua
+++ nvim/lua/user/plugins-config/oil/functions.lua
@@ -5,12 +5,12 @@ function _G.StartOil()
     local buf
 
     buf = vim.api.nvim_get_current_buf()
-    print(buf)
+    -- print(buf)
 
     vim.cmd("vsplit")
 
     buf = vim.api.nvim_get_current_buf()
-    print(buf)
+    -- print(buf)
 end
 
 vim.api.nvim_create_user_command("StartOil", ":lua StartOil()<CR>", { nargs = 0, desc = "Start oil file manager" })
