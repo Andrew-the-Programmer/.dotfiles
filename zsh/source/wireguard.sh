3f512da3686463e72a89b2fe3809977ccc5f13b4
Andrew <andrey.chikin1223@gmail.com>
2025-06-12 21:27:02 +0300

    u

--- zsh/source/wireguard.sh
+++ zsh/source/wireguard.sh
@@ -10,13 +10,29 @@ function wg-get-publickey() {
     sudo cat "$pubkey_file"
 }
 
-wg_connection="wg0"
+wg_connection=""
 eth_connection=$(ip route | awk '/default/ {print $5}' | head -n 1)
 wg_config_file="$wg_dir/$wg_connection.conf"
 
-function wg-get-config-file() {
-    print "$wg_dir/$wg_connection.conf"
+function wg-select-connection() {
+    connections=$(sudo find "$wg_dir" -maxdepth 1 -type f -name "*.conf" -printf "%f\n" | sed 's/\.conf$//')
+    wg_connection=$(
+        print "$connections" |
+            fzf --preview "sudo bat --style=numbers --color=always --line-range :500 $wg_dir/{}.conf" --prompt="Select wg connection: "
+    )
 }
+
+function wg-get-connection() {
+    if [ -z "$wg_connection" ]; then
+        wg-select-connection
+    fi
+    print "$wg_connection"
+}
+
+function wg-get-config-file() {
+    print "$wg_dir/$(wg-get-connection).conf"
+}
+
 function wg-read-config() {
     sudo bat "$(wg-get-config-file)"
 }
@@ -25,7 +41,7 @@ function wg-edit-config() {
 }
 
 function wg_m() {
-    sudo systemctl "$1" "wg-quick@$wg_connection"
+    sudo systemctl "$1" "wg-quick@$(wg-get-connection)"
 }
 function wg-start() {
     wg_m start
