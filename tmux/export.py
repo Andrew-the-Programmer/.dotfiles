a9474b28d70f332f311dff2a9ee6eb841e6652e5
Andrew <andrey.chikin1223@gmail.com>
2026-08-13 16:44:38 +0300

    tmux: clone tpm

--- tmux/export.py
+++ tmux/export.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from utils2 import config, get_cwd_path, get_system_config_path
 
 register = config.register("tmux")
@@ -7,4 +9,12 @@ register = config.register("tmux")
 def tmux_links():
     cwd = get_cwd_path(__file__)
     config = get_system_config_path() / "tmux"
+    _ = subprocess.run(
+        [
+            "git",
+            "clone",
+            "https://github.com/tmux-plugins/tpm",
+            config / "plugins/tpm",
+        ]
+    )
     return [(cwd / "tmux.conf", config / "tmux.conf")]
